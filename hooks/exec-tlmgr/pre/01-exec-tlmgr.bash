5c7b25ae8a1963e58d3f923277ec386c26784e54
redpeacock78 <scarletpeacock78@gmail.com>
2026-04-10 16:57:06 +0900

    feat: Add hooks/exec-tlmgr/pre/01-exec-tlmgr.bash

---
+++ hooks/exec-tlmgr/pre/01-exec-tlmgr.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+declare -r TEXENV_DEBUG
+[[ "${TEXENV_DEBUG}" == 1 ]] && set -x
+set -euo pipefail
+
+main() {
+	case "${2:-}" in
+	install | uninstall | remove | update | info | search)
+		texenv repo >/dev/null
+		;;
+	esac
+}
+
+main "${@}"
